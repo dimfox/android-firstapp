f0c532b17a3a67840c01e85e6ab210090c53fb33
Wei Liu <dimfox@gmail.com>
2013-01-13 22:09:17 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+Android development tutorial
+http://developer.android.com/training/basics/firstapp/index.html
---
+++ gen/R.java.d
@@ -0,0 +1,12 @@
+/Users/weliu/dev/pageplus/pageplus_andrroid/gen/com/example/myfirststep/R.java \
+ : /Users/weliu/dev/pageplus/pageplus_andrroid/res/drawable-hdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/res/drawable-ldpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/res/drawable-mdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/res/drawable-xhdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/res/layout/main.xml \
+/Users/weliu/dev/pageplus/pageplus_andrroid/res/values/strings.xml \
+/Users/weliu/dev/pageplus/pageplus_andrroid/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/bin/res/drawable-ldpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/weliu/dev/pageplus/pageplus_andrroid/bin/AndroidManifest.xml \
